20c90d95062af6ba71cf997488b0aa3f5ba3e2d9
InvincibleSun <103495967+InvincibleSun@users.noreply.github.com>
2022-12-08 13:36:23 +0400

    Added regExp and password-check logic

--- src/app/password/password.component.ts
+++ src/app/password/password.component.ts
@@ -9,5 +9,27 @@ export class PasswordComponent {
   password: string;
   weight: number = 0;
 
-  inputHandler(value: string) {}
+  regExpString = /[a-zA-Z]/g;
+  regExpNumbers = /[0-9]/g;
+  regExpCharacters = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/g;
+
+  inputHandler(value: string) {
+    this.weight = 0;
+
+    if (!value || value.length < 8) {
+      return;
+    }
+
+    if (value.match(this.regExpString)) {
+      this.weight += 1;
+    }
+
+    if (value.match(this.regExpNumbers)) {
+      this.weight += 1;
+    }
+
+    if (value.match(this.regExpCharacters)) {
+      this.weight += 1;
+    }
+  }
 }
